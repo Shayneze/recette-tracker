84ef39bdbe48cafbc906cb6b5d5a1b6b5cb11a4f
Shayneze Menegady <shayneze.menegady@gmail.com>
2025-04-16 15:12:45 +0200

    create methods in recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,22 +1,53 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @recipes = Recipe.all
   end
 
   def show
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+
+    if @recipe.save
+      redirect_to @recipe, notice: "Recette créée avec succès"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: "Recette mise à jour avec succès"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @recipe.destroy
+    redirect_to recipes_path, notice: "Recette supprimée avec succès"
   end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :image)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# 1. Create a user
+user = User.create!(
+  email: "test@example.com",
+  password: "password"
+)
+
+# 2. Clean the database 🗑️
+puts "Cleaning database..."
+Recipe.destroy_all
+
+# 3. Create the instances 🏗️
+puts "Creating recipes..."
+Recipe.create!(
+  [
+    {
+      title: "Pancakes",
+      description: "Délicieux pancakes moelleux",
+      image: "pancakes.jpg",
+      user: user
+    },
+    {
+      title: "Butterchicken",
+      description: "Poulet mijoté avec ses épices. A servir avec du riz ",
+      image: "butterchicken.jpg",
+      user: user
+    },
+    {
+      title: "Gratin dauphinois",
+      description: "Pommes de terre, crème, ail et muscade. À gratiner lentement au four.",
+      image: "gratin.jpg",
+      user: user
+    },
+    {
+      title: "Brownie chocolat",
+      description: "Brownie rapide et efficace. Fondant à souhait",
+      image: "brownie.jpg",
+      user: user
+    },
+    {
+      title: "Pancakes healthy",
+      description: "Pancakes sains aux pépites de chocolat",
+      image: "healthy-pancakes.jpg",
+      user: user
+    }
+  ]
+)
+
+# 4. Display a message 🎉
+puts "Finished! Created #{Recipe.count} recipes."
